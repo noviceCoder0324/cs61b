d0705772bbf97ec5ea40df26fb5197aa60b95fcd
PB <liudaoyu0324@gmail.com>
2021-12-04 15:37:30 -0600

    4th version

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,18 +1,21 @@
-/**Project 1A Data Structures
- * This will use arrays as the core data structure.*/
-
+/**Project 1A Data Structures.
+ * This will use arrays as the core data structure.
+ * @author Daoyu Liu */
 public class ArrayDeque<T> {
 
-    /** instance variables*/
+    /** instance variables. */
     private T[] items;
+    /** size of the list.*/
     private int size;
+    /** first index of the list.*/
     private int firstIndex;
+    /** last index of the list. */
     private int lastIndex;
 
-    /** constant*/
+    /** constant. */
     private static final int START_SIZE = 8;
 
-    /** create an empty list*/
+    /** create an empty list. */
     public ArrayDeque() {
         items = (T[]) new Object[START_SIZE];
         firstIndex = 0;
@@ -20,16 +23,17 @@ public class ArrayDeque<T> {
         size = 0;
     }
 
-    /** addFirst method */
-    public void addFirst(T x){
-        if (size == 0){
+    /** addFirst method.
+     * @param x the item needs to be added. */
+    public void addFirst(T x) {
+        if (size == 0) {
             firstIndex = 0;
             lastIndex = 0;
             items[0] = x;
             size += 1;
             return;
         }
-        if (size == items.length){
+        if (size == items.length) {
             resizeUp();
         }
         if (firstIndex == 0) {
@@ -41,8 +45,9 @@ public class ArrayDeque<T> {
         size += 1;
     }
 
-    /** addLast method*/
-    public void addLast(T x){
+    /** addLast method.
+     * @param x the item needs to be added.*/
+    public void addLast(T x) {
         if (size == 0) {
             firstIndex = 0;
             lastIndex = 0;
@@ -50,7 +55,7 @@ public class ArrayDeque<T> {
             size += 1;
             return;
         }
-        if (size == items.length){
+        if (size == items.length) {
             resizeUp();
         }
         if (lastIndex == items.length - 1) {
@@ -62,17 +67,21 @@ public class ArrayDeque<T> {
         size += 1;
     }
 
-    /** get something from the list. this is smart*/
+    /** get something from the list. this is smart.
+     * @param index the index of the item.
+     * @return the item.*/
     public T get(int index) {
         return items[(firstIndex + index) % items.length];
     }
 
-    /** check if it is empty*/
+    /** check if it is empty.
+     * @return true if the list is empty.*/
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /** return the size*/
+    /** return the size.
+     * @return the size of the list.*/
     public int size() {
         if (size <= 0) {
             return 0;
@@ -80,79 +89,80 @@ public class ArrayDeque<T> {
         return size;
     }
 
-    /** print out the outcome for the Deque*/
+    /** print out the outcome for the Deque. */
     public void printDeque() {
         for (int i = 0; i < items.length; i++) {
             System.out.print(items[i] + " ");
         }
     }
 
-    /** remove the first from the list*/
+    /** remove the first from the list.
+     * @return the item.*/
     public T removeFirst() {
         if (size == 0) {
             return null;
         }
         T removeNode = items[firstIndex];
         items[firstIndex] = null;
-        if (firstIndex == items.length-1){
+        if (firstIndex == items.length - 1) {
             firstIndex = 0;
-        }else{
+        } else {
             firstIndex += 1;
         }
-        size -=1;
-        if (size == 0){
+        size -= 1;
+        if (size == 0) {
             firstIndex = 0;
             lastIndex = 0;
         }
-        if (size < items.length/4) {
+        if (size < items.length / 4) {
             resizeDown();
         }
         return removeNode;
     }
 
-    /** remove the last from the list*/
+    /** remove the last from the list.
+     * @return the item.*/
     public T removeLast() {
         if (size == 0) {
             return null;
         }
         T removeNode = items[lastIndex];
         items[lastIndex] = null;
-        if (lastIndex == 0){
-            lastIndex = items.length-1;
-        }else {
-            lastIndex -=1;
+        if (lastIndex == 0) {
+            lastIndex = items.length - 1;
+        } else {
+            lastIndex -= 1;
         }
-        size -=1;
+        size -= 1;
         if (size == 0) {
             firstIndex = 0;
             lastIndex = 0;
         }
-        if (size < items.length/4) {
+        if (size < items.length / 4) {
             resizeDown();
         }
         return removeNode;
     }
 
-    /** resize the list and make it bigger*/
+    /** resize the list and make it bigger. */
     private void resizeUp() {
-        T[] resizeArray = (T[]) new Object[items.length *2];
-        //copy array into new array
+        T[] resizeArray = (T[]) new Object[items.length * 2];
         int sizeOfFirstCopy = items.length - firstIndex;
         System.arraycopy(items, firstIndex, resizeArray, 0, sizeOfFirstCopy);
-        System.arraycopy(items, 0, resizeArray, sizeOfFirstCopy, size-sizeOfFirstCopy);
+        System.arraycopy(items, 0, resizeArray, sizeOfFirstCopy, size - sizeOfFirstCopy);
         items = resizeArray;
         firstIndex = 0;
-        lastIndex = size -1;
+        lastIndex = size - 1;
     }
 
-    /** resize down the list */
+    /** resize down the list. */
     private void resizeDown() {
         T[] resizeArray = (T[]) new Object[items.length / 2];
         if (lastIndex < firstIndex) {
             int sizeOfFirstCopy = items.length - firstIndex;
             System.arraycopy(items, firstIndex, resizeArray, 0, sizeOfFirstCopy);
-            System.arraycopy(items, 0, resizeArray, sizeOfFirstCopy, size-sizeOfFirstCopy);
-        }else{
+            System.arraycopy(items, 0, resizeArray, sizeOfFirstCopy, size - sizeOfFirstCopy);
+        } else {
             System.arraycopy(items, firstIndex, resizeArray, 0, size);
         }
         items = resizeArray;
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,65 +1,65 @@
-import junit.framework.Test;
 
-import javax.swing.plaf.TabbedPaneUI;
-
-/** Project 1A Data Structures
+/** Project 1A Data Structures.
  * This is Linked list based deque Implementation
  * Q 1 Linked List Deque:
- * I answer this question by checking gitbook chapter 2.3*/
-public class LinkedListDeque <T> {
+ * I answer this question by checking gitbook chapter 2.3
+ * @author PB */
+public class LinkedListDeque<T> {
 
 
-    /** Basic list structure*/
+    /** Basic list structure. */
     private class Node {
-        /** instance variable for every IntNode*/
-        public Node prev;
-        public T item;
-        public Node next;
-        /** constructor*/
-        public Node(T i, Node p, Node n) {
+        /** instance variable for every IntNode. */
+        private Node prev;
+        /** the item itself. */
+        private T item;
+        /** next node in the list. */
+        private Node next;
+        /** constructor.
+         * @param i the item.
+         * @param p the previous node.
+         * @param n the next node.*/
+        private Node(T i, Node p, Node n) {
             this.prev = p;
             this.item = i;
             this.next = n;
         }
     }
 
-    /** Instance variables*/
+    /** Instance variables. */
     private Node sentinel;
+    /** the size of the list. */
     private int size;
 
-    /** Constructor */
-    public LinkedListDeque(T x) {
-        /* why am I able to use null as the item for sentinel???*/
-        sentinel = new Node ((T) null, null, null);
-        sentinel.next = new Node (x, sentinel, sentinel);
-        sentinel.prev = sentinel.next;
-        size = 1;
-    }
+    /** Constructor. */
     public LinkedListDeque() {
         /* why do I need to define separately? why can't I put sentinel in
         * p and n as parameter directly???*/
-        sentinel = new Node ((T) null, null, null);
+        sentinel = new Node((T) null, null, null);
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
         size = 0;
     }
 
-    /** Basic rules
-     * add a new item at the beginning of the list*/
+    /** Basic rules.
+     * add a new item at the beginning of the list
+     * @param x the item.*/
     public void addFirst(T x) {
-        this.sentinel.next = new Node (x, sentinel, sentinel.next);
+        this.sentinel.next = new Node(x, sentinel, sentinel.next);
         this.sentinel.next.next.prev = this.sentinel.next;
         size += 1;
     }
 
-    /** add a new item at the end of the list*/
+    /** add a new item at the end of the list.
+     * @param x the item to be added. */
     public void addLast(T x) {
-        sentinel.prev = new Node (x, sentinel.prev, sentinel);
+        sentinel.prev = new Node(x, sentinel.prev, sentinel);
         sentinel.prev.prev.next = sentinel.prev;
         size += 1;
     }
 
-    /** remove an item at the beginning of the list*/
+    /** remove an item at the beginning of the list.
+     * @return  the item just be removed.*/
     public T removeFirst() {
         if (size == 0) {
             return null;
@@ -71,7 +71,8 @@ public class LinkedListDeque <T> {
         return result;
     }
 
-    /** remove an item at the end of the list*/
+    /** remove an item at the end of the list.
+     * @return  the item just be removed. */
     public T removeLast() {
         if (size == 0) {
             return null;
@@ -83,12 +84,14 @@ public class LinkedListDeque <T> {
         return result;
     }
 
-    /** get the item at index i */
+    /** get the item at index i.
+     * @param n the index of the item
+     * @return the item at n position.*/
     public T get(int n) {
         if (n < size) {
             Node p = sentinel;
             int i = 0;
-            while ( i <= n){
+            while (i <= n) {
                 p = p.next;
                 i += 1;
             }
@@ -97,24 +100,32 @@ public class LinkedListDeque <T> {
         return sentinel.item;
     }
 
-    /** get the item at index i recursively*/
+    /** get the item at index i recursively.
+     *  @param n the index of the item.
+     *  @return the item.*/
     public T getRecursive(int n) {
-        return get_recur_helper(n, sentinel);
+        return getRecursive(n, sentinel);
     }
-    private T get_recur_helper(int n, Node p){
+    /** helper function.
+     * @param n the index of the item.
+     * @param p the pointer.
+     * @return the item.*/
+    private T getRecursive(int n, Node p) {
         if (n == 0) {
             return p.next.item;
-        }else{
-            return get_recur_helper(n-1, p.next);
+        } else {
+            return getRecursive(n - 1, p.next);
         }
     }
 
-    /** return the size of the list*/
+    /** return the size of the list.
+     * @return the size of the list. */
     public int size() {
         return this.size;
     }
 
-    /** return if a list is empty */
+    /** return if a list is empty.
+     * @return true if the list is empty.*/
     public boolean isEmpty() {
         if (size <= 0) {
             return true;
@@ -122,13 +133,13 @@ public class LinkedListDeque <T> {
         return false;
     }
 
-    /** print the list from first to last, separated by a space */
-    public void printDeque(){
+    /** print the list from first to last, separated by a space. */
+    public void printDeque() {
         if (size > 0) {
             String result = "";
             Node p = sentinel;
             int i = 0;
-            while (i < size){
+            while (i < size) {
                 result = result + p.next.item + " ";
                 p = p.next;
                 i += 1;
